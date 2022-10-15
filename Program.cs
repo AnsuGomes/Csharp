dae284ee3c540993a58d22eccec6dd19433ae6b8
AnsuGomes <gomesdasilva1996@gmail.com>
2022-10-15 14:50:57 -0300

    Lista de JSON

--- Program.cs
+++ Program.cs
@@ -2,14 +2,21 @@
 using Newtonsoft.Json;
 using System.IO;
 using System;
+using System.Collections.Generic;
 
-class Program
+internal class Program
 {
     private static void Main(string[] args)
     {
+        List<Venda> listaVendas = new List<Venda>();
+      
         Venda v1 = new Venda(1, "Material de escritorio", 25.00M);
+        Venda v2 = new Venda(2, "Computador", 250.00M);
 
-        string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+        listaVendas.Add(v1);
+        listaVendas.Add(v2);
+
+        string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
         File.WriteAllText("models/vendas.json", serializado);
 
