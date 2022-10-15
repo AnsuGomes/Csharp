bc1cb4a8ffe678eceaee6b0d3b9e4e850fb2bb2d
AnsuGomes <gomesdasilva1996@gmail.com>
2022-10-15 14:31:28 -0300

    Primeira versão de Json

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ExemploJson;
+using Newtonsoft.Json;
+using System.IO;
+using System;
+
+class Program
+{
+    private static void Main(string[] args)
+    {
+        Venda v1 = new Venda(1, "Material de escritorio", 25.00M);
+
+        string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+
+        File.WriteAllText("models/vendas.json", serializado);
+
+        Console.WriteLine(serializado);
+    }
+}
